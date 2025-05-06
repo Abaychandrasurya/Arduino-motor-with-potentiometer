a9218242512c8781ce9c5222cce83556ea18e9ab
Abaychandrasurya J K <abhayc2274@gmail.com>
2025-05-06 19:44:02 +0530

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,16 @@
+const int LEDpin = 9;
+const int potpin = A0;
+int pot;
+int speed;
+
+void setup()
+{
+
+}
+
+void loop()
+{
+  pot = analogRead(potpin);
+  speed = map(pot,0,1023,0,255);
+  analogWrite(LEDpin,speed);
+}
